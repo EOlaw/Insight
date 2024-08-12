248780645460edb29b6ccfa036f882e054f8ab79
EOlaw <Emmanuel.ao@outlook.com>
2024-08-12 04:15:16 -0500

    updated userUtils.js

--- cc/src/auth/utils/userUtils.js
+++ cc/src/auth/utils/userUtils.js
@@ -1,136 +1,108 @@
 // Import the necessary modules.
 const User = require('../models/userModel');
-const Consultant = require('../../consult/models/consultantModel')
-const Client = require('../../consult/models/clientModel')
+const Consultant = require('../../consult/models/consultantModel');
+const Client = require('../../consult/models/clientModel');
 const passport = require('passport');
-/*
-function checkContractorRole(req, res, next) {
-    if (req.user.role === 'contractor') {
-        return next(); // Allow access to the route
-    } else {
-        res.status(403).send('Access denied. You are not allowed to access.');
-    }
-}
-*/
 
 // Middleware to check if user is authenticated
 function isAuthenticated(req, res, next) {
-    if (!req.isAuthenticated()) {
-        req.session.returnTo = req.originalUrl
-        //req.flash('error', 'You must be signed in first!')
-        return res.redirect('/insightserenity/user/login')
+    if (req.isAuthenticated()) {
+        return next();
     }
-    next()
+    req.session.returnTo = req.originalUrl;
+    return res.status(401).json({ message: 'Authentication required' });
 }
 
 // Middleware to check if user is admin
 function authorizeAdmin(req, res, next) {
-    // Check if user is authenticated
     if (req.isAuthenticated()) {
-        // Retrieve the user from the database
         User.findById(req.user._id)
             .then((user) => {
                 if (user && user.isAdmin) {
-                    // If the user is admin, allow access to the route
                     next();
                 } else {
-                    // If the user is not admin, deny access with status 403
                     console.log('User is not admin. Access denied.');
-                    return res.status(403).send('Access denied. You are not allowed to access.');
+                    return res.status(403).json({ message: 'Access denied. You are not allowed to access.' });
                 }
             })
             .catch((err) => {
                 console.error('Error while checking admin status:', err);
-                return res.status(500).send('Internal Server Error');
+                return res.status(500).json({ message: 'Internal Server Error' });
             });
     } else {
-        // If the user is not authenticated, redirect to the login page
-        console.log('User is not authenticated. Redirecting to /user/login');
-        return res.status(401).json({ error: 'You must be logged in to access this resource'})
-        //res.redirect('/');
+        console.log('User is not authenticated. Authentication required.');
+        return res.status(401).json({ message: 'Authentication required' });
     }
 }
 
 // Middleware to check if user is authorized as consultant
 function authorizeConsultant(req, res, next) {
-    // Check if user is authenticated
     if (req.isAuthenticated()) {
-        // Retrieve the user from the database
         User.findById(req.user._id)
             .then((user) => {
                 if (user && user.role === 'consultant') {
-                    // If the user is a consultant, allow access to the route
                     next();
                 } else {
-                    // If the user is not a consultant, deny access with status 403
                     console.log('User is not authorized as consultant. Access denied.');
-                    return res.status(403).send('Access denied. You are not allowed to access.');
+                    return res.status(403).json({ message: 'Access denied. You are not allowed to access.' });
                 }
             })
             .catch((err) => {
                 console.error('Error while checking authorization as consultant:', err);
-                return res.status(500).send('Internal Server Error');
+                return res.status(500).json({ message: 'Internal Server Error' });
             });
     } else {
-        // If the user is not authenticated, redirect to the login page
-        console.log('User is not authenticated. Redirecting to /user/login');
-        return res.status(401).json({ error: 'You must be logged in to access this resource' })
-        //res.redirect('/user/login');
+        console.log('User is not authenticated. Authentication required.');
+        return res.status(401).json({ message: 'Authentication required' });
     }
 }
 
 // Middleware to check if user is authorized as client
 function authorizeClient(req, res, next) {
-    // Check if user is authenticated
     if (req.isAuthenticated()) {
-        // Retrieve the user from the database
         User.findById(req.user._id)
             .then((user) => {
                 if (user && user.role === 'client') {
-                    // If the user is a client, allow access to the route
                     next();
                 } else {
-                    // If the user is not a client, deny access with status 403
-                    res.status(403).send('Access denied. You are not allowed to access.');
+                    return res.status(403).json({ message: 'Access denied. You are not allowed to access.' });
                 }
             })
             .catch((err) => {
                 console.error('Error while checking authorization as client:', err);
-                res.status(500).send('Internal Server Error');
+                return res.status(500).json({ message: 'Internal Server Error' });
             });
     } else {
-        // If the user is not authenticated, redirect to the login page
-        console.log('User is not authenticated. Redirecting to /user/login');
-        return res.status(401).render('error',{ error: 'You must be logged in to access this resource' })
-        // res.status(401).redirect('/insightserenity/user/login');
+        console.log('User is not authenticated. Authentication required.');
+        return res.status(401).json({ message: 'Authentication required' });
     }
 }
 
-// Middleware to check if user is authorized as client
+// Middleware to check if user is authorized as developer
 function authorizeDeveloper(req, res, next) {
-    // Check if user is authenticated
     if (req.isAuthenticated()) {
-        // Retrieve the user from the database
         User.findById(req.user._id)
             .then((user) => {
-                if (user && user.role === 'developer') {
-                    // If the user is a client, allow access to the route
+                if (user && user.isDeveloper) {
                     next();
                 } else {
-                    // If the user is not a client, deny access with status 403
-                    res.status(403).send('Access denied. You are not allowed to access.');
+                    return res.status(403).json({ message: 'Access denied. You are not allowed to access.' });
                 }
             })
             .catch((err) => {
-                console.error('Error while checking authorization as client:', err);
-                res.status(500).send('Internal Server Error');
+                console.error('Error while checking authorization as developer:', err);
+                return res.status(500).json({ message: 'Internal Server Error' });
             });
     } else {
-        // If the user is not authenticated, redirect to the login page
-        console.log('User is not authenticated. Redirecting to /user/login');
-        return res.status(401).render('error',{ error: 'You must be logged in to access this resource' })
-        // res.status(401).redirect('/insightserenity/user/login');
+        console.log('User is not authenticated. Authentication required.');
+        return res.status(401).json({ message: 'Authentication required' });
     }
 }
 
-module.exports = { isAuthenticated, authorizeAdmin, authorizeConsultant, authorizeClient, authorizeDeveloper }
+module.exports = { 
+    isAuthenticated, 
+    authorizeAdmin, 
+    authorizeConsultant, 
+    authorizeClient, 
+    authorizeDeveloper 
+};
